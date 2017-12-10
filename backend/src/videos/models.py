919633397bd7e51ae2a15592ba67f5c4d66e3b40
Алексей Белов <Alenorze@gmail.com>
2017-12-10 23:47:02 +0200

    Video Model

--- backend/src/videos/models.py
+++ backend/src/videos/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+
+class Video(models.Model):
+    name      = models.Charfield(max_length=220)
+    slug      = models.SlugField(unique=True, blank=True)
+    embed     = models.Charfield(max_length=120, null=True, blank=True)
+    featured  = models.BooleandField(default=False)
+    updated   = models.DateTimeField(auto_now=True)
+    timestamp = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name 
