2a63dcf0b1289aa8c02f32cf4aa80240d374c23d
Алексей Белов <Alenorze@gmail.com>
2017-12-10 20:20:08 +0200

    Django 2.0

--- backend/src/imger/settings.py
+++ backend/src/imger/settings.py
@@ -49,7 +49,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-ROOT_URLCONF = 'tryangular.urls'
+ROOT_URLCONF = 'imger.urls'
 
 TEMPLATES = [
     {
@@ -67,7 +67,7 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = 'tryangular.wsgi.application'
+WSGI_APPLICATION = 'imger.wsgi.application'
 
 
 # Database
