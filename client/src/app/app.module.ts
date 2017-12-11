e8b42b0cefc7234a1c12fd8611225e91ba508de3
Алексей Белов <Alenorze@gmail.com>
2017-12-11 20:26:05 +0200

    Catch All 404 & 500 Server Page

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -3,30 +3,32 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
-// third party imports
 import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
 import { CarouselModule } from 'ngx-bootstrap/carousel';
-
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app.routing';
 import { HomeComponent } from './home/home.component';
+import { NotFoundComponent } from './not-found/not-found.component';
 import { SafePipe } from './utility/safe.pipe';
 import { SearchComponent } from './search/search.component';
 import { SearchDetailComponent } from './search-detail/search-detail.component';
-import { VideoListComponent } from './video-list/video-list.component';
 import { VideoDetailComponent } from './video-detail/video-detail.component';
+import { VideoListComponent } from './video-list/video-list.component';
+
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    VideoListComponent,
-    VideoDetailComponent,
     SafePipe,
     HomeComponent,
+    NotFoundComponent,
+    SafePipe,
     SearchComponent,
-    SearchDetailComponent
+    SearchDetailComponent,
+    VideoDetailComponent,
+    VideoListComponent,
   ],
   imports: [
     // ngx-bootstrap
